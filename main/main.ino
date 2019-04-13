093b077d3c6983919a4385101e9d2999973c9cfd
Tobias Franek <tobias.franek@gmail.com>
2019-04-13 08:20:55 +0200

    test

--- main/main.ino
+++ main/main.ino
@@ -8,17 +8,37 @@ MeEncoderNew motor3(0x12, SLOT1);
 MeEncoderNew motor4(0x12, SLOT2);
 
 bool _dir = false;
-int _speed = 50;
+int _speed = 30;
 int _desiredSpeed;
+
+float ultraAngle = 30.0;
+
+const float pi = 3.14;
+
+float coneDistance;
+
+int delayTime = 100;
+
+bool searchForBlackLine = true;
+bool foundItem = false;
+bool firstTime = true;
 
 MePort port(PORT_5);
 Servo myservo1;  // create servo object to control a servo 
 int16_t servo1pin =  port.pin1();//attaches the servo on PORT_3 SLOT1 to the servo object
 
-MeUltrasonicSensor ultraSensor(PORT_10); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
+MeUltrasonicSensor ultraSensorLeft(PORT_10); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
+MeUltrasonicSensor ultraSensorForward(PORT_9);
+
+MeLineFollower lineFinderLeft(PORT_8);
+MeLineFollower lineFinderForward(PORT_7);
+MeLineFollower lineFinderRight(PORT_6);
+
 
 void setup()
 {
+  ultraAngle = (30.0/180.0)*pi;
+  
   initWheels();
   myservo1.attach(servo1pin);  // attaches the servo on servopin1
   Serial.begin(9600);
@@ -31,14 +51,14 @@ void initWheels() {
   motor4.begin();
 }
 
-void moveForward(int s) {
+void moveSideways(int s) {
   motor1.runSpeed(s);
   motor2.runSpeed(-s);
   motor3.runSpeed(-s);
   motor4.runSpeed(s);
 }
 
-void moveSideways(int s) {
+void moveForward(int s) {
   motor1.runSpeed(-s);
   motor2.runSpeed(-s);
   motor3.runSpeed(s);
@@ -52,26 +72,84 @@ void resetSpeed() {
   motor4.runSpeed(0);
 }
 
+void spinAround(int s) {
+  motor1.runSpeed(s);
+  motor2.runSpeed(s);
+  motor3.runSpeed(s);
+  motor4.runSpeed(s);
+}
+
 void loop()
 {
-  if(_dir){
-    _desiredSpeed = _speed;
-  }else{
-    _desiredSpeed = -_speed;
+  delayTime = 50;
+  // - speed right / + speed left
+  // - speed backwards / + speed forward
+  int lineFollowerLeftState = lineFinderLeft.readSensors();
+  int lineFollowerForwardState = lineFinderForward.readSensors();
+  int lineFollowerRightState = lineFinderRight.readSensors();
+
+  //reached black line
+  if(lineFollowerLeftState == S1_IN_S2_IN && searchForBlackLine == true) {
+    resetSpeed();
+    // if (firstTime) {
+    //   moveSideways(_speed);
+    //   delay(400);
+    //   resetSpeed(); 
+    //   firstTime = false; 
+    // }
+
+    searchForBlackLine = false;
+  }
+  
+  if(searchForBlackLine == true) {
+    resetSpeed();
+    moveSideways(-30);
+  } else {
+    float itemDistance = ultraSensorLeft.distanceCm();
+    if (lineFollowerForwardState == S1_IN_S2_IN) {
+      resetSpeed();
+    } else if(itemDistance < 100 || foundItem == true) {
+      if (foundItem == false) {
+        foundItem = true;
+        resetSpeed();
+        moveForward(_speed);
+        if(itemDistance > 8 && itemDistance < 25) {
+          delayTime = 300;
+        } else if (itemDistance > 25 && itemDistance < 45) {
+          delayTime = 700;
+        } else if (itemDistance > 45 && itemDistance < 70) {
+          delayTime = 1200;
+        } else  {
+          delayTime = 1200;
+        }
+      } else if (ultraSensorLeft.distanceCm() < 8) {
+        resetSpeed();
+        // execute grap logic
+      } else {
+        if(lineFollowerRightState == S1_IN_S2_IN) {
+          searchForBlackLine = true;
+          foundItem = false;
+          resetSpeed();
+        } else {
+          resetSpeed();
+          moveSideways(_speed);
+        }
+      }
+    } else if (ultraSensorForward.distanceCm() < 8) {
+      resetSpeed();
+      moveForward(-_speed);
+      delay(300);
+      spinAround(-_speed);
+      delay(2600);
+      // execute grap logic
+    } else {
+      if(foundItem == false) {
+        moveForward(_speed);
+      }
+    }
   }
 
-  // myservo1.write(0);                  
-  // delay(2000);                           
-  // myservo1.write(180);
-  // delay(2000);
-  Serial.println(ultraSensor.distanceCm());
-  myservo1.write(ultraSensor.distanceCm());
-  delay(100);
 
-  // moveForward(_desiredSpeed);
-  // delay(3000);
-  // resetSpeed();
-  // delay(1000);
 
-  _dir = !_dir;
+  delay(delayTime);
 }
